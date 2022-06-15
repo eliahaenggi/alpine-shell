699930bab5e3be6d6829ad9f6b4b95cce12b149d
Elia Haenggi <elia.haenggi@stud.unibas.ch>
2022-06-15 09:51:05 +0200

    add commands in help.c

--- commands/help.c
+++ commands/help.c
@@ -14,5 +14,7 @@ int printHelp() {
     printf(" \033[1;36mcopy\033[0m                         -- copy file\n");
     printf(" \033[1;36mcopy <old_path> <new_path>\033[0m   -- choose path to copy file\n");
     printf(" \033[1;36mrun <command>\033[0m                -- execute operating system command\n");
+    printf(" \033[1;36mhelp<command>\033[0m                -- get command informations\n");
+    printf(" \033[1;36mexit<command>\033[0m                -- quit shell\n");
     return 0;
 }
